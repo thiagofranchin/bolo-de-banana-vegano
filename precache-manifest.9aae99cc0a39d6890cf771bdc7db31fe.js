58698788cf123543fc8c91f50e3120793f74bb64
Thiago Franchin <thiago.franchin@gmail.com>
2019-08-17 18:37:32 -0300

    dist for gh-pages

---
+++ precache-manifest.9aae99cc0a39d6890cf771bdc7db31fe.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "65e3be4eea08191c64040fbcb8006068",
+    "url": "/bolo-de-banana-vegano/fonts/fa-brands-400.65e3be4e.woff2"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bolo-de-banana-vegano/robots.txt"
+  },
+  {
+    "revision": "030846b7226a63373512cbd2c71ff51a",
+    "url": "/bolo-de-banana-vegano/fonts/fa-brands-400.030846b7.woff"
+  },
+  {
+    "revision": "1035c52e77058dd570e8",
+    "url": "/bolo-de-banana-vegano/js/app.c67007cd.js"
+  },
+  {
+    "revision": "7779d7cb7382399fcfef",
+    "url": "/bolo-de-banana-vegano/js/chunk-vendors.56cb4b63.js"
+  },
+  {
+    "revision": "47d19eca4372c3489b27f39f512e3a07",
+    "url": "/bolo-de-banana-vegano/img/fa-regular-400.47d19eca.svg"
+  },
+  {
+    "revision": "8add58ae3c3d62ec2a57",
+    "url": "/bolo-de-banana-vegano/js/about.f4ab49ed.js"
+  },
+  {
+    "revision": "3f5a250e01aa2a2c757c8d8915216e0b",
+    "url": "/bolo-de-banana-vegano/fonts/fa-brands-400.3f5a250e.ttf"
+  },
+  {
+    "revision": "d30b80185b2bd2b99ddcfde903a49a50",
+    "url": "/bolo-de-banana-vegano/fonts/fa-regular-400.d30b8018.eot"
+  },
+  {
+    "revision": "813b8aee60f235b36887a388b70e1359",
+    "url": "/bolo-de-banana-vegano/fonts/fa-solid-900.813b8aee.woff2"
+  },
+  {
+    "revision": "140f41485edce6f713abe17625eba4c1",
+    "url": "/bolo-de-banana-vegano/fonts/fa-solid-900.140f4148.woff"
+  },
+  {
+    "revision": "72d11b217f20449734918dff5d5ebb9c",
+    "url": "/bolo-de-banana-vegano/index.html"
+  },
+  {
+    "revision": "8e04d338e5f3a734136e5fa058f60d0b",
+    "url": "/bolo-de-banana-vegano/img/fa-brands-400.8e04d338.svg"
+  },
+  {
+    "revision": "e98a92ac980c63c46a7e25c4fbcf30ef",
+    "url": "/bolo-de-banana-vegano/img/fa-solid-900.e98a92ac.svg"
+  },
+  {
+    "revision": "e57e108a1ae04ca2b27cab75e4478867",
+    "url": "/bolo-de-banana-vegano/fonts/fa-solid-900.e57e108a.ttf"
+  },
+  {
+    "revision": "4910ec733558f59bf05834d4f831a48d",
+    "url": "/bolo-de-banana-vegano/fonts/fa-solid-900.4910ec73.eot"
+  },
+  {
+    "revision": "914437d606603d81e81a52e9e9b704b5",
+    "url": "/bolo-de-banana-vegano/fonts/fa-regular-400.914437d6.woff2"
+  },
+  {
+    "revision": "6bf2d6c8c5f78cb9fc035dc7c2b1253e",
+    "url": "/bolo-de-banana-vegano/fonts/fa-regular-400.6bf2d6c8.ttf"
+  },
+  {
+    "revision": "222941bf21f9c9cb93bace95e9171d39",
+    "url": "/bolo-de-banana-vegano/fonts/fa-regular-400.222941bf.woff"
+  },
+  {
+    "revision": "4291d4b6c28b411e821a90adb4716fd7",
+    "url": "/bolo-de-banana-vegano/fonts/fa-brands-400.4291d4b6.eot"
+  },
+  {
+    "revision": "7779d7cb7382399fcfef",
+    "url": "/bolo-de-banana-vegano/css/chunk-vendors.26c863c1.css"
+  },
+  {
+    "revision": "1035c52e77058dd570e8",
+    "url": "/bolo-de-banana-vegano/css/app.98fe4e13.css"
+  }
+];
