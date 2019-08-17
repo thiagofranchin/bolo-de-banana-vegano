d349b1112b4ec46f7a08f61bf89cf424f12d894e
Thiago Franchin <thiago.franchin@gmail.com>
2019-08-17 18:16:28 -0300

    Bootstrap 4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+    <div class="row">
+      <div class="col-12">
+        <h1>Bolo de banana vegano</h1>
+        <h3>Ingredientes</h3>
+        <ul>
+          <li>2 colheres de sopa de fermento químico</li>
+          <li>1 colher de café de bicarbonato</li>
+          <li>Misturar geral</li>
+          <li>Cortar algumas fatias de banana para colocar em cima do bolo</li>
+          <li>Esmagar 6 bananas</li>
+          <li>Untar forma com manteiga e jogar farinha</li>
+          <li>1½ xícara de farinha integral</li>
+          <li>1½ xícara de farinha branca</li>
+          <li>Misture as farinhas</li>
+          <li>1 colher de sopa de canela</li>
+          <li>2 duas colheres de café de sal</li>
+          <li>1 xícara de açúcar mascavo</li>
+          <li>1 xícara de coco ralado sem açúcar</li>
+          <li>Misturar tudo</li>
+          <li>1 colher de café de vinagre</li>
+          <li>Abrir buraco na farinha e colocar 1 xícara de óleo de girassol 200ml</li>
+          <li>Jogar banana no buraco onde foi colocado o óleo</li>
+          <li>Mexer pouco até misturar</li>
+          <li>Pré-aquecer o forno e colocar 40 minutos no mínimo</li>
+          <li>Após 40 minutos colocar no fogo alto e deixar mais 10 minutos</li>
+        </ul>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
